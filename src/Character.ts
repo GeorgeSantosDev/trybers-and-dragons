012dde2962c93638bbc75cab742e06039048990a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-18 20:40:41 -0300

    feat: To implement getters in Character class

--- src/Character.ts
+++ src/Character.ts
@@ -11,20 +11,19 @@ export default class Character implements Fighter {
   private _defense: number;
   private _dexterity: number;
   private _energy: Energy;
+  private _race: Race;
+  private _archetype: Archetype; 
+  private _name: string;
 
-  constructor(
-    private _name: string,
-    private _race?: Race, 
-    private _archetype?: Archetype, 
-  ) {
-    this._name = _name;
+  constructor(name: string, race?: Race, archetype?: Archetype) {
+    this._name = name;
     this._dexterity = getRandomInt(1, 10);
 
     const defaultRace = new Elf(this._name, this._dexterity);
-    this._race = _race || defaultRace; 
+    this._race = race || defaultRace;
 
     const defaultArchetype = new Mage(this._name);
-    this._archetype = _archetype || defaultArchetype;
+    this._archetype = archetype || defaultArchetype;
 
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
@@ -35,5 +34,29 @@ export default class Character implements Fighter {
       type_: this._archetype.energyType,
       amount: getRandomInt(1, 10),
     };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
   }
 }
